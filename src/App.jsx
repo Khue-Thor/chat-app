052831a4fcbbc381f2034694e5202ab38b35ca27
khuephamy <khuephumy19@gmail.com>
2023-05-08 10:36:22 -0500

    init chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className='app'>
-      app
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
