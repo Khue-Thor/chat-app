7ae67918fbc038412a2a2b4b7be47ef7ac9dea42
khuephamy <khuephumy19@gmail.com>
2023-05-12 15:50:37 -0500

    updated some css

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from "react-chat-engine-advanced";
 import Header from "../Header/Header";
 import StandardMessageForm from "../CustomMessageForms/StandardMessageForm";
+import Ai from "../CustomMessageForms/Ai";
 
 const Chat = () => {
   const chatProps = useMultiChatLogic(
@@ -18,6 +19,9 @@ const Chat = () => {
         style={{ height: "100vh" }}
         renderChatHeader={(chat) => <Header chat={chat} />}
         renderMessageForm={(props) => {
+          if (chatProps.chat?.title.startsWith("AiChat_")) {
+            return <Ai props={props} activeChat={chatProps.chat} />;
+          }
           return <StandardMessageForm props={props} activeChat={chatProps.chat} />;
         }}
       />
--- src/components/CustomMessageForms/StandardMessageForm.jsx
+++ src/components/CustomMessageForms/StandardMessageForm.jsx
@@ -1,6 +1,5 @@
 import { PaperAirplaneIcon, PaperClipIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import React, { useState } from "react";
-import Dropzone from "react-dropzone";
 import MessageFormUI from "./MessageFormUI";
 
 const StandardMessageForm = ({ props, activeChat }) => {
