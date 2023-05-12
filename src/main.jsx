0011b1f42de48e364fd7fe389fbfdcf268a47645
khuephamy <khuephumy19@gmail.com>
2023-05-12 11:09:44 -0500

    update Main

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query/index.js";
+import { api } from "@/state/api";
+
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+
+setupListeners(store.dispatch);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
